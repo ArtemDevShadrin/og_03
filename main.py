e4fc3ec5294a7922bb9f0ac1fabf80f82af61391
ArtemDevShadrin <160567918+ArtemDevShadrin@users.noreply.github.com>
2024-03-25 15:12:54 +0700

    add all cycle code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame
 import random
+
+import pygame
 
 pygame.init()
 
@@ -34,8 +35,24 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 #################################################################
 
+#################################################################
 running = True
 while running:
-    pass
-
+    # зальем экран цветом нашим случайным цветом
+    screen.fill(color)
+    # получаем все события в игре
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                # задаем случайные координаты цели
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    # отрисовываем нашу мишень
+    screen.blit(target_img, (target_x, target_y))
+    # обновление экрана
+    pygame.display.update()
+#################################################################
 pygame.quit()
